6e83e88bb9e1f68c46c67c67b40f8293550df39e
Aixa Filsinger <macafil99@gmail.com>
2023-05-22 08:02:30 -0300

    se agrego el maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { Container } from 'postcss';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,7 +8,14 @@ function App() {
 
   return (
     <>
-     
+     <Container className="my-5 mainPage">
+      <h1 className='dispkay-4 text-center text-light'>Lista de tareas</h1>
+     <hr className='text-light'/>
+     </Container>
+    
+    <footer className='bg-dark'>
+          &copy Todos los derechos reservados
+    </footer>
     </>
   )
 }
