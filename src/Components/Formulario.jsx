b95aed77f683e477f1dfd1bfe66e9f156d4069d5
Aixa Filsinger <macafil99@gmail.com>
2023-05-22 08:29:05 -0300

    agregamos los componentes listatarea y itemtarea

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,27 +1,20 @@
 
 import {Form, Button} from 'react-bootstrap'
+import ListaTarea from './ListaTarea';
 const Formulario = () => {
     return (
-        <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          Well never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+        <section>
+             <Form>
+      <Form.Group className="mb-3 d-flex" controlId="tarea">
+        <Form.Control type="text" placeholder="Ingrese una tarea" />
       <Button variant="primary" type="submit">
-        Submit
-      </Button>
+        Enviar
+      </Button> 
+      </Form.Group>
     </Form>
+    <ListaTarea></ListaTarea>
+        </section>
+       
     );
 };
 
---
+++ src/Components/ItemTarea.jsx
@@ -0,0 +1,11 @@
+
+import { Button, ListGroup } from "react-bootstrap";
+const ItemTarea = () => {
+    return (
+        <ListGroup.Item>Cras justo odio
+            <Button variant="danger">Borrar</Button>
+        </ListGroup.Item>
+    );
+};
+
+export default ItemTarea;
