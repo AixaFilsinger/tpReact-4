e37be11ed68274a6467a665c40eb5aab79fbfe3c
Aixa Filsinger <macafil99@gmail.com>
2023-05-22 08:16:48 -0300

    errores corregidos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Container } from 'postcss';
+import { Container } from 'react-bootstrap';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Formulario from './Components/Formulario';
@@ -12,12 +12,13 @@ function App() {
      <Container className="my-5 mainPage">
       <h1 className='dispkay-4 text-center text-light'>Lista de tareas</h1>
      <hr className='text-light'/>
-     </Container>
      <Formulario></Formulario>
+     </Container>
+     
     
-    <footer className='bg-dark'>
-          &copy Todos los derechos reservados
-    </footer>
+     <footer className="bg-dark text-light text-center py-4">
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     </>
   )
 }
