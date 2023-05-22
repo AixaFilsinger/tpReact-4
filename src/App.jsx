f15e9180e4ac38496bd5a3a5d169130a824e71c9
Aixa Filsinger <macafil99@gmail.com>
2023-05-22 08:08:43 -0300

    se agrego un form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Container } from 'postcss';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Formulario from './Components/Formulario';
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
       <h1 className='dispkay-4 text-center text-light'>Lista de tareas</h1>
      <hr className='text-light'/>
      </Container>
+     <Formulario></Formulario>
     
     <footer className='bg-dark'>
           &copy Todos los derechos reservados
